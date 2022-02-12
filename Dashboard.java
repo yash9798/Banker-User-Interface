bf0689ddca7aa36a0df2dc2060cf980c3a5afd22
yash9798 <41871239+yash9798@users.noreply.github.com>
2022-02-12 10:25:13 -0500

    Update Dashboard.java
    - removed redundant code (visDash is unnecessary due to cardLayout)

--- Dashboard.java
+++ Dashboard.java
@@ -1,20 +1,12 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 public class Dashboard extends JPanel {
-	
-	public Dashboard()
-	{
+	public Dashboard() {
 		this.setBounds(100, 100, 500, 250);
 		this.setVisible(false);
 		JLabel poo = new JLabel("yas");
 		poo.setBounds(200,200,100,200);
 		this.add(poo);
-	}
-	
-	public void setVisDash()
-	{
-		this.setVisible(true);
 	}
 }
