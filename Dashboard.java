950e51bf2c2b3ed0bac6dfaada7e1b6b79ba2523
its-obviouz <99286427+its-obviouz@users.noreply.github.com>
2022-02-15 14:54:33 -0500

    Update Dashboard.java

--- Dashboard.java
+++ Dashboard.java
@@ -1,16 +1,22 @@
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Dashboard extends JPanel {
-	
-	public Dashboard()
+	ArrayList<BankAccount> accs;;
+	public Dashboard(ArrayList<BankAccount> b) throws IOException
 	{
+		accs=b;
 		this.setBounds(100, 100, 500, 250);
 		this.setVisible(false);
 		this.setLayout(new GridBagLayout());
@@ -20,28 +26,45 @@ public class Dashboard extends JPanel {
 		gbc.gridy = 0;
 		gbc.anchor = gbc.WEST;
 		gbc.insets = new Insets(10,10,10,10);
-		JLabel bank = new JLabel("Bank accounts: "); // + num bank accounts
+		JLabel bank = new JLabel("Bank accounts: " + accs.size()); // + num bank accounts
 		add(bank,gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 1;
 		gbc.anchor = gbc.WEST;
 		gbc.insets = new Insets(10,10,10,10);
-		JLabel check = new JLabel("Checking accounts: "); // + num checkings accounts
+		int c = 0;
+		int r = 0;
+		int m = 0;
+		for(int i = 0; i < accs.size(); i++)
+		{
+			
+			if(accs.get(i) instanceof CheckingAccount)
+					{
+		c++;
+					}
+			if(accs.get(i) instanceof SavingsAccount)
+			{
+				r++;
+			}
+			
+			m += accs.get(i).getBalance();
+		}
+		JLabel check = new JLabel("Checking accounts: " +c); // + num checkings accounts
 		add(check,gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 2;
 		gbc.anchor = gbc.WEST;
 		gbc.insets = new Insets(10,10,10,10);
-		JLabel savings = new JLabel("Savings accounts: "); // + num savings accounts
+		JLabel savings = new JLabel("Savings accounts: " + r); // + num savings accounts
 		add(savings,gbc);
 		
 		gbc.gridx = 0;
 		gbc.gridy = 3;
 		gbc.anchor = gbc.WEST;
 		gbc.insets = new Insets(10,10,10,10);
-		JLabel moners = new JLabel("Moners deposited "); // + moners
+		JLabel moners = new JLabel("Moners deposited $" + m); // + moners
 		add(moners,gbc);
 		
 		gbc.gridx = 1;
@@ -49,6 +72,25 @@ public class Dashboard extends JPanel {
 		gbc.anchor = gbc.CENTER;
 		gbc.insets = new Insets(10,10,10,10);
 		JButton end = new JButton("End of Month Update"); // + moners
+		end.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String password = JOptionPane.showInputDialog("Enter a password: ");
+				if(password.equals("password"))
+				{
+					for(int i = 0; i < accs.size(); i++)
+					{
+						
+//						if(accs.get(i) instanceof CheckingAccount)
+//								{
+//							accs.get(i).endOfMonthUpdate();
+//								}
+//						if(accs.get(i) instanceof SavingsAccount)
+//						{
+							accs.get(i).endOfMonthUpdate();
+//						}
+					}
+				}
+			}});
 		add(end,gbc);
 		
 		gbc.gridx = 1;
